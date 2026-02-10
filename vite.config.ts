6b827fe1fec5836067551f1c3fa017cd3aaabc1c
goncalofm90 <goncalofm90@gmail.com>
2026-02-10 15:50:42 +0000

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,    
+    },
+  },
 })
